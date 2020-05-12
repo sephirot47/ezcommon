7fe1e2c6fe371d68381598e6cfd33781f61bca5d
sephirot47 <victorantondominguez@gmail.com>
2020-05-12 21:41:09 +0200

    Added more CommonTypeTraits

--- include/ez/CommonTypeTraits.h
+++ include/ez/CommonTypeTraits.h
@@ -1,9 +1,26 @@
 #pragma once
 
 #include "ez/Span.h"
+#include <array>
+#include <cstdlib>
 
 namespace ez
 {
+// IsArray
+template <typename T>
+struct IsArray : std::false_type
+{
+};
+
+template <typename T, std::size_t N>
+struct IsArray<std::array<T, N>> : std::true_type
+{
+};
+
+template <typename T>
+static constexpr auto IsArray_v = IsArray<T>::value;
+
+// IsSpan
 template <typename T>
 struct IsSpan : std::false_type
 {
