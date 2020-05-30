86136916cf64e2af52bec39db838f92ffb7a3003
sephirot47 <victorantondominguez@gmail.com>
2020-05-30 17:47:37 +0200

    Added Variant function

--- include/ez/CommonTypeTraits.h
+++ include/ez/CommonTypeTraits.h
@@ -3,6 +3,7 @@
 #include "ez/Span.h"
 #include <array>
 #include <cstdlib>
+#include <variant>
 
 namespace ez
 {
@@ -33,4 +34,22 @@ struct IsSpan<Span<T>> : std::true_type
 
 template <typename T>
 static constexpr auto IsSpan_v = IsSpan<T>::value;
+
+// VariantIndex
+template <typename TVariantType, typename T, std::size_t TIndex>
+constexpr std::size_t VariantIndex()
+{
+  if constexpr ((TIndex == std::variant_size_v<TVariantType>)
+      || std::is_same_v<std::variant_alternative_t<TIndex, TVariantType>, T>)
+  {
+    return TIndex;
+  }
+  else
+  {
+    return VariantIndex<TVariantType, T, TIndex + 1>();
+  }
+}
+
+template <typename TVariantType, typename T>
+static constexpr auto VariantIndex_v = VariantIndex<TVariantType, T, 0>();
 }
